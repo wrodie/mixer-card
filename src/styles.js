f0125f2ba3e293b76f37b88eb446e1d334c52cfe
warren <warren@rodie.net>
2026-05-17 13:35:24 +1000

    revert css nesting for better compatibility

--- src/styles.js
+++ src/styles.js
@@ -50,142 +50,137 @@ export const mixerCardStyles = css`
     }
 
     /* Orientation  - Vertical */
-    .fader-orientation-vertical {
 
-        & .fader {
-            padding: 6px 10px;
-        }
-        & .fader-value {
-            margin-top: 10px;
-            text-align: center;
-        }
-        & .fader-name {
-            margin-top: 30px;
-            text-align: center;
-            display: block;
-            font-weight: 300;
-            text-align: center;
-            font-size:14px;
-            text-transform: capitalize;
-        }
+    .fader-orientation-vertical .fader {
+        padding: 6px 10px;
+    }
+    .fader-orientation-vertical .fader-value {
+        margin-top: 10px;
+        text-align: center;
+    }
+    .fader-orientation-vertical .fader-name {
+        margin-top: 30px;
+        text-align: center;
+        display: block;
+        font-weight: 300;
+        text-align: center;
+        font-size:14px;
+        text-transform: capitalize;
+    }
 
-        & .active-button {
-            margin:20px;
-            margin-top: 30px;
-            line-height:20px;
-            border: 1px solid #bbb;
-            box-shadow: 1px 1px 1px #bbb;
-            display:block;
-            padding: 5px;
-            cursor:pointer;
-            vertical-align: center;
-            text-align: center;
-            border-radius: 5px;
-        }
-        & .range-holder {
-            height: var(--fader-height);
-            width: var(--fader-width);
-            position:relative;
-            display: block;
-            margin-right: auto;
-            margin-left: auto;
-        }
-        
-        & .range-holder input[type="range"] {
-            margin: 0;
-            outline: 0;
-            border: 0;
-            -webkit-transform:rotate(270deg);
-            -moz-transform:rotate(270deg);
-            -o-transform:rotate(270deg);
-            -ms-transform:rotate(270deg);
-            transform:rotate(270deg);
-            position: absolute;
-            top: calc(50% - (var(--fader-width) / 2));
-            right: calc(50% - (var(--fader-height) / 2));
-            background-color: var(--fader-track-color);
-            transition: box-shadow 0.2s ease-in-out;
-            -webkit-appearance: none;
-            appearance: none;
-            border-radius: var(--fader-border-radius, 12px);
-        }
+    .fader-orientation-vertical .active-button {
+        margin:20px;
+        margin-top: 30px;
+        line-height:20px;
+        border: 1px solid #bbb;
+        box-shadow: 1px 1px 1px #bbb;
+        display:block;
+        padding: 5px;
+        cursor:pointer;
+        vertical-align: center;
+        text-align: center;
+        border-radius: 5px;
+    }
+    .fader-orientation-vertical .range-holder {
+        height: var(--fader-height);
+        width: var(--fader-width);
+        position:relative;
+        display: block;
+        margin-right: auto;
+        margin-left: auto;
+    }
+
+    .fader-orientation-vertical .range-holder input[type="range"] {
+        margin: 0;
+        outline: 0;
+        border: 0;
+        -webkit-transform:rotate(270deg);
+        -moz-transform:rotate(270deg);
+        -o-transform:rotate(270deg);
+        -ms-transform:rotate(270deg);
+        transform:rotate(270deg);
+        position: absolute;
+        top: calc(50% - (var(--fader-width) / 2));
+        right: calc(50% - (var(--fader-height) / 2));
+        background-color: var(--fader-track-color);
+        transition: box-shadow 0.2s ease-in-out;
+        -webkit-appearance: none;
+        appearance: none;
+        border-radius: var(--fader-border-radius, 12px);
     }
 
     /* Orientation  - Horizontal */
-    .fader-orientation-horizontal {
 
-        & .fader-holder {
-            display: contents;
-        }
-        & .fader {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            padding: 0;
-            margin-bottom: 0px;
-            gap: 15px;
-        }
-        & .fader-holder,
-        & .fader-data {
-            display: contents;
-        }
-        & .fader-value {
-            text-align: center;
-            display:inline-block;
-            order: 3;
-            min-width: 50px;
-        }
-        & .fader-name {
-            display:inline-block;
-            text-align: center;
-            font-weight: 300;
-            text-align: left;
-            font-size:14px;
-            text-transform: capitalize;
-            order: 4;
-        } 
-        & .active-button-holder {
-            display:inline-block;
-            order: 1;
-        }
-        & .active-button {
-            line-height:20px;
-            border: 1px solid #bbb;
-            box-shadow: 1px 1px 1px #bbb;
-            display: inline-block;
-            padding: 5px;
-            cursor:pointer;
-            vertical-align: center;
-            text-align: center;
-            border-radius: 5px;
-        }
+    .fader-orientation-horizontal .fader-holder {
+        display: contents;
+    }
+    .fader-orientation-horizontal .fader {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0;
+        margin-bottom: 0px;
+        gap: 15px;
+    }
+    .fader-orientation-horizontal .fader-holder,
+    .fader-orientation-horizontal .fader-data {
+        display: contents;
+    }
+    .fader-orientation-horizontal .fader-value {
+        text-align: center;
+        display:inline-block;
+        order: 3;
+        min-width: 50px;
+    }
+    .fader-orientation-horizontal .fader-name {
+        display:inline-block;
+        text-align: center;
+        font-weight: 300;
+        text-align: left;
+        font-size:14px;
+        text-transform: capitalize;
+        order: 4;
+    }
+    .fader-orientation-horizontal .active-button-holder {
+        display:inline-block;
+        order: 1;
+    }
+    .fader-orientation-horizontal .active-button {
+        line-height:20px;
+        border: 1px solid #bbb;
+        box-shadow: 1px 1px 1px #bbb;
+        display: inline-block;
+        padding: 5px;
+        cursor:pointer;
+        vertical-align: center;
+        text-align: center;
+        border-radius: 5px;
+    }
 
-        & .range-holder {
-            order: 2;
-            height: var(--fader-width);
-            width: var(--fader-height);
-            position:relative;
-            display: flex;
-            align-items: center;
-            margin: 0;
-        }
-    
-        & .range-holder input[type="range"] {
-            margin: 0;
-            outline: 0;
-            border: 0;
-            top: 50%;
-            position: absolute;
-            transform: translateY(-50%);
-            left: 0;
-            height: var(--fader-width);
-            background-color: var(--fader-track-color);
-            transition: box-shadow 0.2s ease-in-out;
-            -webkit-appearance: none;
-            appearance: none;
-            border-radius: var(--fader-border-radius, 12px);
-        }
+    .fader-orientation-horizontal .range-holder {
+        order: 2;
+        height: var(--fader-width);
+        width: var(--fader-height);
+        position:relative;
+        display: flex;
+        align-items: center;
+        margin: 0;
+    }
 
+    .fader-orientation-horizontal .range-holder input[type="range"] {
+        margin: 0;
+        outline: 0;
+        border: 0;
+        top: 50%;
+        position: absolute;
+        transform: translateY(-50%);
+        left: 0;
+        height: var(--fader-width);
+        background-color: var(--fader-track-color);
+        transition: box-shadow 0.2s ease-in-out;
+        -webkit-appearance: none;
+        appearance: none;
+        border-radius: var(--fader-border-radius, 12px);
     }
 
     /* Theme Physical */
